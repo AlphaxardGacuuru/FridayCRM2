70dcecb0cf4f6ebc3a679203a19ef87b0a836077
Alphaxard Gacuuru <alphaxardgacuuru47@gmail.com>
2023-11-28 14:17:07 +0300

    Formated date picker to include time

--- public/assets/libs/js/date-range-picker.js
+++ public/assets/libs/js/date-range-picker.js
@@ -1,7 +1,7 @@
 $(function () {
 	$('input[name="daterange"]').daterangepicker({
 		locale: {
-			format: "YYYY-MM-DD",
+			format: "YYYY-MM-DD H:m:s",
 		},
 		// startDate: moment().startOf("month"),
 		// endDate: moment().endOf("month"),
